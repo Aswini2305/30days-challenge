75a25908187deeab78779b9ec8e3c1306bd37213
Aswini2305 <72400882+Aswini2305@users.noreply.github.com>
2020-11-23 19:39:02 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,22 @@
+#printing the statement
+print("30 days and 30 hours challenge")
+#intialise the variable with the string 
+Hours ="Implementation"
+print(Hours)
+#slicing
+print(Hours[3])
+print(Hours[2:4])
+#length of string
+print(len(Hours)) 
+#string to lower case
+print(Hours.lower())
+Seconds="Testing"
+Time=Hours+Seconds
+print(Time)
+print(Hours+" "+Seconds)
+X=Hours.casefold()
+print(X)
+#print from second character
+A="Hardwork and smartwork"
+print(A[1:])
+
