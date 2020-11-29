cc02ba219786dda20e6d7a796ade589a266ddc52
Aswini2305 <72400882+Aswini2305@users.noreply.github.com>
2020-11-29 16:43:32 +0530

    Add files via upload

---
+++ Day7.py
@@ -0,0 +1,21 @@
+#excercise 1my module
+list1=[10,20,64,37,21]
+#imported python file
+import modulo
+a=modulo.list1
+print(a)
+x=a.insert(1,'23')
+print(a)
+#excercise 2
+#pandas installed successfully
+#excercise 3 random integer
+import random
+print("Random integer is",random.randint(0,100))
+#excercise 4
+import sys
+a=sys.path
+print(a)
+#excercise 5
+pip install six  #to install six
+pip uninstall six #to uninstall six
+
